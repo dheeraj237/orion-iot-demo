09ad3c00685f3821ea2d6767b18d50a0e8a4f286
root <dheerusuthar@gmail.com>
2019-10-07 02:10:03 +0530

    commit: docs updated

--- deploy-to-git.sh
+++ deploy-to-git.sh
@@ -1,4 +1,5 @@
-ng build --prod --output-path docs --base-href orion-iot-demo
+rm -rf docs
+ng build --prod --output-path docs --base-href "https://anothergitdev.github.io/orion-iot-demo/"
 cp ./docs/index.html ./docs/404.html
 git add .
 git commit -m "commit: docs updated"
